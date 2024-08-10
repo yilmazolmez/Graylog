2b1ddffaf4aab25c804880841d4bbf9019345ddf
Yılmaz Ölmez <yilmazlmez@gmail.com>
2024-08-10 17:14:17 +0300

    ApplicationModule Added

---
+++ Graylog.API/Modules/ApplicationModule.cs
@@ -0,0 +1,12 @@
+﻿using Graylog.Application;
+
+namespace Graylog.API.Modules
+{
+    public static class ApplicationModule
+    {
+        public static void AddApplicationModule(this IServiceCollection services)
+        {
+            services.AddScoped<IGraylogService, GraylogService>();
+        }
+    }
+}
