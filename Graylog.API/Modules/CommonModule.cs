58cb88ae484063fda35c3c7e800289f0db8408a7
Yılmaz Ölmez <yilmazlmez@gmail.com>
2024-08-10 17:13:39 +0300

    CommonModule Added

---
+++ Graylog.API/Modules/CommonModule.cs
@@ -0,0 +1,12 @@
+﻿using Graylog.Common.LogEvents;
+
+namespace Graylog.API.Modules
+{
+    public static class CommonModule
+    {
+        public static void AddCommonModule(this IServiceCollection services)
+        {
+            services.AddScoped<ILogEvent, LogEvent>();
+        }
+    }
+}
