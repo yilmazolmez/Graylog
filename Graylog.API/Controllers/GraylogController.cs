d116ba1d3d7534e1d7ef022b3888c9bddabe4b95
Yılmaz Ölmez <yilmazlmez@gmail.com>
2024-08-10 17:29:30 +0300

    example API project completed

--- Graylog.API/Controllers/GraylogController.cs
+++ Graylog.API/Controllers/GraylogController.cs
@@ -1,3 +1,4 @@
+using Graylog.Application;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Graylog.API.Controllers
@@ -6,11 +7,28 @@ namespace Graylog.API.Controllers
     [Route("[controller]")]
     public class GraylogController : ControllerBase
     {
+        private readonly IGraylogService _graylogService;
+
+        public GraylogController(IGraylogService graylogService)
+        {
+            _graylogService = graylogService;
+        }
+
+
         [HttpGet]
         public async Task<IActionResult> Get()
         {
+            var response = await _graylogService.GetAsync();
 
-            return Ok();
+            return Ok(response);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] string requestMessage)
+        {
+            var response = await _graylogService.PostAsync(requestMessage);
+
+            return Ok(response);
         }
 
     }
--- Graylog.Application/GraylogService.cs
+++ Graylog.Application/GraylogService.cs
@@ -3,15 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace Graylog.Application
 {
     public interface IGraylogService
     {
-
+        public Task<List<string>> GetAsync();
+        public Task<string> PostAsync(string message);
     }
 
     public class GraylogService : IGraylogService
     {
+        public Task<List<string>> GetAsync()
+        {
+            List<string> data = new List<string> { "Data 1", "Data 2", "Data 3" };
+
+            return Task.FromResult(data);
+        }
+
+        public Task<string> PostAsync(string message)
+        {
+            return Task.FromResult($"message:{message}");
+        }
     }
 }
