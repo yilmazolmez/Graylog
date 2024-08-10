e35ba9073624d87d8d172cb919076bf538cedbbc
Yılmaz Ölmez <yilmazlmez@gmail.com>
2024-08-10 17:14:56 +0300

    LoggingModule Added

---
+++ Graylog.API/Modules/LoggingModule.cs
@@ -0,0 +1,17 @@
+﻿using NLog.Extensions.Logging;
+
+namespace Graylog.API.Modules
+{
+    public static class LoggingModule
+    {
+        public static void AddLoggingModule(this IServiceCollection services)
+        {
+            services.AddLogging(loggingBuilder =>
+            {
+                loggingBuilder.ClearProviders();
+                loggingBuilder.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Warning);
+                loggingBuilder.AddNLog();
+            });
+        }
+    }
+}
